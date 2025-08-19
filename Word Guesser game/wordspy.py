7743eda70b1665e6d7f4ea21add32c90c328b6cc
Bishesh <bkhadgi7@gmail.com>
2025-08-19 21:01:36 +0545

    New repo

--- Word Guesser game/wordspy.py
+++
@@ -1,9 +0,0 @@
-print("WELCOME TO WORD SPY!!!")
-
-number_of_players = int(input("Enter the number of players:"))
-players = []
-
-for i in range(number_of_players):
-    player_name = input(f"Enter name of player {i+1}:")
-    players.append(player_name)
-    
